0631d353b7e70a016d45ee4754b1623657b62b07
jihunhong <redgee49@gmail.com>
2023-04-18 00:49:43 +0900

    Edit headless option

--- src/lib/puppeteer.ts
+++ src/lib/puppeteer.ts
@@ -3,7 +3,7 @@ import { USER_AGENT } from '../config';
 
 export async function launchBrowser(): Promise<[Browser, Page]> {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
   const page = await browser.newPage();
   await page.setUserAgent(USER_AGENT);
@@ -14,7 +14,7 @@ export async function launchBrowser(): Promise<[Browser, Page]> {
  * 크롤링시 딜레이 주기위해 이용하는 함수.
  * 1.5초 + 2.xx랜덤초를 딜레이
  * @param ms 1500 defaultValue
- * @returns 
+ * @returns
  */
 export const delay = (ms: number = 1500): Promise<void> => {
   return new Promise(res => {
