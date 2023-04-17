6534142619374440f4a6de8c4c4554561f72d79e
jihunhong <redgee49@gmail.com>
2023-04-18 01:17:31 +0900

    Add Accept-Language Header

--- src/lib/puppeteer.ts
+++ src/lib/puppeteer.ts
@@ -3,7 +3,7 @@ import { USER_AGENT } from '../config';
 
 export async function launchBrowser(): Promise<[Browser, Page]> {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
   const page = await browser.newPage();
   await page.setUserAgent(USER_AGENT);
